2af55658215ef404ea479f723aaf639aa2bf9850
GiorgioSimonini <75115636+GiorgioSimonini@users.noreply.github.com>
2024-06-03 10:49:10 +0000

    node ok, to test

---
+++ scripts/throwing_par.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+# license removed for brevity
+import numpy as np
+from math import log, sin, cos, atan2
+from scipy.optimize import minimize
+from scipy.spatial.transform import Rotation as rot
+import yaml
+
+import rospy
+# from std_msgs.msg import String
+from handtool_throw.srv import throwing_par_srv, throwing_par_srvResponse
+from std_msgs.msg import UInt32
+from geometry_msgs.msg import Pose, Point, Quaternion
+
+# --- global parameters --- #
+dist_base = 0.5 # plant distance from base
+h_base = 0.4	# tool height
+valve_0 = 0.051
+g = 9.81
+bound_valve = (0.051, 0.3)
+bound_theta = (-3.1415/2, 3.1415/2)
+IG_valve = 0.052
+IG_theta = 0.0
+    
+# ----- functions ----- #
+def fun_energy(x, x0):
+    x = float(x)
+    x0 = float(x0)
+    if x > x0:
+        # identified parameters
+        a1 = 2.0
+        a2 = 1.0
+        a3 = -0.05092
+        b = 20.0
+        y = float(a1*log(a2*x+a3, b) - a1*log(a2*x0+a3, b))
+    else:
+        y = 0.0
+    return y
+
+def R_x(angle):
+    R = np.matrix(((1, 0, 0), (0, cos(angle), -sin(angle)), (0, sin(angle), cos(angle))))
+    return R
+
+def R_y(angle):
+    R = np.matrix(((cos(angle), 0, sin(angle)), (0, 1, 0), (-sin(angle), 0, cos(angle))))
+    return R
+
+def R_z(angle):
+    R = np.matrix(((cos(angle), -sin(angle), 0), (sin(angle), cos(angle), 0), (0, 0, 1)))
+    return R
+
+def get_target_info(target):
+    d_xy_target = target - np.array([0, 0, target[2]])
+    pos_tool = d_xy_target/np.linalg.norm(d_xy_target)*dist_base
+    pos_tool[2] = h_base
+    
+    dist = np.linalg.norm(d_xy_target) - dist_base
+    
+    h = h_base - target[2]
+    return [dist, h, pos_tool]
+
+def get_landing(m_obj, valve_dt, h, theta):
+    v_obj = np.sqrt(2 * fun_energy(valve_dt, valve_0) / m_obj)
+    distance = complex(v_obj*cos(theta)/g * ( v_obj * sin(theta) + np.sqrt(v_obj**2 * sin(theta)**2 + 2*g*h)))
+    return distance.real
+
+def objective(x, par):
+    m_obj = par[0]
+    target = par[1]
+    [dist_desired, h, pos_tool]  = get_target_info(target)
+    valve_dt = x[0]
+    theta = x[1]
+    dist = get_landing(m_obj, valve_dt, h, theta)
+    obj = (dist_desired-dist)**2
+    return obj
+
+# def constraint(x):
+#     return x[0] - 0.051
+
+def get_throwing_par(m_obj, target):
+    [target_dist, h, pos_tool] = get_target_info(target)
+    # - solve optimization - #
+    # - parameters - #
+    par = (m_obj, target) # tuple of parameters to pass
+    # initial guesses
+    x0 = np.zeros(2)
+    x0[0] = IG_valve
+    x0[1] = IG_theta
+    
+    # show initial objective
+    print('Initial SSE Objective: ' + str(objective(x0, par)))
+    
+    # optimize
+    bnds = (bound_valve, bound_theta)
+    # con1 = {'type': 'ineq', 'fun': constraint}
+    # cons = ([con1])
+    # solution = minimize(objective, x0, args=(par,), method='SLSQP', bounds=bnds, constraints=cons)
+    solution = minimize(lambda x : objective(x, par), x0,  method='SLSQP', bounds=bnds)
+    x = solution.x
+    valve_dt = x[0]
+    theta = x[1]
+    # show final objective
+    print('Final SSE Objective: ' + str(objective(x, par)))
+    
+    # print solution
+    print('Solution')
+    print('x1 = ' + str(x[0]))
+    print('x2 = ' + str(x[1]))
+    dist = get_landing(m_obj, valve_dt, h, theta)
+    print('distance: ' + str(dist))
+
+    # get R from theta
+    angle_z = atan2(target[1], target[0])
+    angle_y = -3.1415-theta
+    R = np.matmul(R_z(angle_z), R_y(angle_y))
+    
+    return [valve_dt, pos_tool, R]
+
+# ----- handtool server node ----- #
+def callback_throwing_par(req):
+    m_obj = req.m_obj
+    target = np.array([req.target.x, req.target.y, req.target.z])
+    [valve_dt, pos_tool, R] = get_throwing_par(m_obj, target)
+    valve_us = int(valve_dt*1e6)
+    r = rot.from_matrix(R)
+    print("R: ")
+    print(R)
+    quat = r.as_quat()
+    pose = Pose()
+    pose.position.x = pos_tool[0]
+    pose.position.y = pos_tool[1]
+    pose.position.z = pos_tool[2]
+    pose.orientation.x = quat[0]
+    pose.orientation.y = quat[1]
+    pose.orientation.z = quat[2]
+    pose.orientation.w = quat[3]
+    return throwing_par_srvResponse(result_valve_us = valve_us, result_pose = pose)
+
+def handtool_server():
+    rospy.init_node('handtool_server')
+    s = rospy.Service('throwing_par_srv', throwing_par_srv, callback_throwing_par)
+    # get parameters from yaml
+    # with open('../config/handtool_parameters.yaml', 'r') as file:
+    #     handtool_params = yaml.safe_load(file)
+    global dist_base
+    global h_base
+    global valve_0
+    global g
+    global bound_valve
+    global bound_theta
+    global IG_valve
+    global IG_theta
+    # dist_base = handtool_params['optimization']['dist_base']
+    # h_base = handtool_params['optimization']['h_base']
+    # valve_0 = handtool_params['optimization']['valve_0']
+    # g = handtool_params['optimization']['g']
+    # bound_valve = handtool_params['optimization']['bound_valve']
+    # bound_theta = handtool_params['optimization']['bound_theta']
+    # IG_valve = handtool_params['optimization']['IG_valve']
+    # IG_theta = handtool_params['optimization']['IG_theta']
+    dist_base = rospy.get_param('optimization/dist_base')
+    h_base = rospy.get_param('optimization/h_base')
+    valve_0 = rospy.get_param('optimization/valve_0')
+    g = rospy.get_param('optimization/g')
+    bound_valve = rospy.get_param('optimization/bound_valve')
+    bound_theta = rospy.get_param('optimization/bound_theta')
+    IG_valve = rospy.get_param('optimization/IG_valve')
+    IG_theta = rospy.get_param('optimization/IG_theta')
+    
+    # spin the node
+    rospy.spin()
+
+if __name__ == "__main__":
+    handtool_server()
--- src/testing.cpp
+++ src/testing.cpp
@@ -2,9 +2,11 @@
 #include <std_msgs/Empty.h>
 #include <std_msgs/UInt8.h>
 #include <std_msgs/UInt32.h>
+#include "handtool_throw/throwing_par_srv.h"
 
 // #include <tf/transform_broadcaster.h>
-// #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Point.h>
 // #include <eigen_conversions/eigen_msg.h>
 
 // #include<tf_conversions/tf_eigen.h>
@@ -16,10 +18,10 @@ std_msgs::Empty empty_msg;
 std_msgs::UInt32 valve_msg;
 std_msgs::UInt8 regulator_msg;
 
-float pre_suct_time = 2;
-float suct_time = 0.5;
-unsigned int valve_us = 50000; 	// valve time opering in microseconds
-unsigned int duty_regulator = 255; // 255: bypass regulator
+double pre_suct_time = 2.0;
+double suct_time = 0.5;
+int valve_us = 50000; 	// valve time opering in microseconds
+int duty_regulator = 255; // 255: bypass regulator
 
 
 int main(int argc, char **argv)
@@ -32,12 +34,29 @@ int main(int argc, char **argv)
 	ros::Publisher pub_suct = nh_.advertise<std_msgs::Empty>("arduino/suctioning", 1); 
 	ros::Publisher pub_reg = nh_.advertise<std_msgs::UInt8>("arduino/duty_cycle", 1); 
 	ros::Publisher pub_valve = nh_.advertise<std_msgs::UInt32>("arduino/valve", 1);
+	// handtool service
+	ros::ServiceClient handtool_client = nh_.serviceClient<handtool_throw::throwing_par_srv>("throwing_par_srv");
+	handtool_throw::throwing_par_srv srv;
+	// parameters from yaml
+	if (!nh_.getParam("/testing/pre_suct_time", pre_suct_time)) {
+		ROS_WARN("Failed to get param");
+	}
+	if (!nh_.getParam("/testing/suct_time", suct_time)) {
+		ROS_WARN("Failed to get param");
+	}
+	if (!nh_.getParam("/testing/valve_us", valve_us)) {
+		ROS_WARN("Failed to get param");
+	}
+	if (!nh_.getParam("/testing/duty_regulator", duty_regulator)) {
+		ROS_WARN("Failed to get param");
+	}
+	// wait
 	ros::Duration(1.0).sleep();
 
 	std::cout << "Welcome to the hand-tool testing"<< std::endl;
 	int choice;
 	while(ros::ok()){
-		cout<<"choice:   (1: set times (pre-suct,suct),  2: set regulator,  3: set valve time (us),  4: throw loop) "<<endl;
+		cout<<"choice:   (1: set times (pre-suct,suct),  2: set regulator,  3: set valve time (us),  4: throw loop,  5: get_throw_par) "<<endl;
 		cin>>choice;
 		// while (!ready) ros::spinOnce();
 		if (choice == 1){
@@ -45,21 +64,18 @@ int main(int argc, char **argv)
 			cin >> pre_suct_time;
 			cin >> suct_time;
 			ros::Duration(0.1).sleep();
-			cout << "ok!" << endl;
 		}else if (choice == 2){
 			// --- set duty-cycle of regulator --- //
 			cin >> duty_regulator;
 			regulator_msg.data = duty_regulator;
 			pub_reg.publish(regulator_msg);
 			ros::Duration(0.1).sleep();
-			cout << "ok!" << endl;
 		}else if (choice == 3){
 			// --- set valve time --- //
 			cin >> valve_us;
 			valve_msg.data = valve_us;
 			pub_valve.publish(valve_msg);
 			ros::Duration(0.1).sleep();
-			cout << "ok!" << endl;
 		}else if (choice == 4){
 			// --- set duty-cycle of regulator --- //
 			// wait
@@ -69,6 +85,38 @@ int main(int argc, char **argv)
 			ros::Duration(suct_time).sleep();
 			// throwing
 			pub_throw.publish(empty_msg);
+			ros::Duration(0.1).sleep();
+		}else if (choice == 5){
+			// --- get parameters for handtool throwing --- //
+			cout << "insert object weight and target x, y, z positions (m_obj x y z):" << endl;
+			float m_obj, x, y, z;
+			cin >> m_obj;
+			cin >> x;
+			cin >> y;
+			cin >> z;
+			srv.request.m_obj = m_obj;
+			geometry_msgs::Point target;
+			target.x = x;
+			target.y = y;
+			target.z = z;
+			srv.request.target = target;
+			if (handtool_client.call(srv)){
+				unsigned int valve_opt = srv.response.result_valve_us;
+				geometry_msgs::Pose pose = srv.response.result_pose;
+
+				std::cout << "Valve time in us:" << valve_opt << std::endl;
+				std::cout << "Position:" << std::endl;
+				std::cout << "  x: " << pose.position.x << std::endl;
+				std::cout << "  y: " << pose.position.y << std::endl;
+				std::cout << "  z: " << pose.position.z << std::endl;
+				std::cout << "Orientation:" << std::endl;
+				std::cout << "  x: " << pose.orientation.x << std::endl;
+				std::cout << "  y: " << pose.orientation.y << std::endl;
+				std::cout << "  z: " << pose.orientation.z << std::endl;
+				std::cout << "  w: " << pose.orientation.w << std::endl;
+			} else {
+				ROS_ERROR("Failed to call service");
+			}
 			ros::Duration(0.1).sleep();
 		}
 	}
